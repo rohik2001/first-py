ad86f0c095d07fbcbf9a5376f4b9eea55982b204
rohik2001 <mdhabibshayim2001@gmail.com>
2024-11-02 21:01:38 +0600

    Add files via upload

---
+++ first class.py
@@ -0,0 +1,27 @@
+#algorithom
+''''print('step 1: step1')
+print('step 2: Declear vereable num1,num2,num3')
+print('step: 3 sum=num1+num2+3')
+print('step: 4 Display sum')
+print('step: 5 stop')
+'''
+num1=55
+num2=75
+sum=num1+num2
+print(sum)
+
+name='Rohik' #string datatype
+grade=9 #intiger datatype
+gpa=4.98 #float datatype
+student=True #bolean datatype
+print(name)
+print("Student Name is: ",name)
+print(f"Student Name is: {name} who red in grade{grade}.He got gpa{gpa}.He is a student{student}")
+
+
+height=20
+leanth=30
+area=height*leanth
+print(f'rectangle : {area}')
+print(area)
+
